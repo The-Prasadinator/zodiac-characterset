191d4781b2e6149778ae0858223a85a629690503
The-Prasadinator <56118198+The-Prasadinator@users.noreply.github.com>
2019-10-04 17:16:51 +0530

    Add zodiac.h main file

---
+++ src/Zodiac.h
@@ -0,0 +1,21 @@
+/* Arithmetic operators */
+#define PRODUCT *
+#define SUM +
+#define QUO /
+#define MIN -
+/* Logical Operators */
+#define START_ARG "("
+#define END_ARG ")"
+#define AND &&
+#define OR ||
+/* Loop Operators */
+#define THEN "{"
+#define END "}"
+#define ADD "#include"
+/* Just for fun */
+#define TER ;
+#define ASSN =
+typedef struct group;
+typedef class libGroup;
+
+
